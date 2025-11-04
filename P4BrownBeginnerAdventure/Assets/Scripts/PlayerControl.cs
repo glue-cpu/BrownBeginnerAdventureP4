33b1ae48c5963069da18890031c3edf4f6b3ccea
glue-cpu <729868@student.dusd.net>
2025-11-04 11:45:23 -0800

    Better movement
    Tried to make better movement. Need to transfer horizontal and vertical variables to real movement.

--- P4BrownBeginnerAdventure/Assets/Scripts/PlayerControl.cs
+++ P4BrownBeginnerAdventure/Assets/Scripts/PlayerControl.cs
@@ -1,20 +1,58 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 public class PlayerControl : MonoBehaviour
 {
     // Start is called before the first frame update
     void Start()
     {
-        
+
     }
 
     // Update is called once per frame
+    float horizontal = 0.0f;
+    float vertical = 0.0f;
     void Update()
     {
-        Vector2 position = transform.position;
-        position.x = position.x + 0.001f;
+        float position = transform.position;
+        position.x = horizontal;
+        position.y = vertical;
         transform.position = position;
+
+        //This is the movement part of the script
+        if (Keyboard.current.leftArrowKey.isPressed)
+        {
+            horizontal = -1.0f;
+        }
+        else if (Keyboard.current.rightArrowKey.isPressed)
+        {
+            horizontal = 1.0f;
+        }
+        Debug.Log(horizontal);
+
+        if (Keyboard.current.upArrowKey.isPressed) { 
+            vertical = 1.0f;
+        }
+        else if (Keyboard.current.downArrowKey.isPressed)
+        {
+            vertical = -1.0f;
+        }
+        Debug.Log(vertical);
+
+
+        //Friction area---------------------
+        
+        //if the player has horizontal movement
+        if (horizontal > 0.0f)
+        {
+            horizontal = horizontal - 0.001f;
+        }
+        //terminates horizontal movement if it's less than 0
+        else if (horizontal <= 0.0f)
+        {
+            horizontal = 0.0f;
+        }
     }
 }
