599f98860529ec01c558f705c8cf0881ff5b3a87
glue-cpu <729868@student.dusd.net>
2025-11-06 11:45:50 -0800

    Made better movement
    Made a better movement system, it's arrow keys.

--- P4BrownBeginnerAdventure/Assets/Scripts/PlayerControl.cs
+++ P4BrownBeginnerAdventure/Assets/Scripts/PlayerControl.cs
@@ -3,56 +3,31 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.InputSystem;
 
+public class PlayerController : MonoBehaviour
+{
+    public InputAction LeftAction;
+    public InputAction MoveAction;
+}
+
 public class PlayerControl : MonoBehaviour
 {
     // Start is called before the first frame update
     void Start()
     {
-
+        //QualitySettings.vSyncCount = 0;
+        //Application.targetFrameRate = 900;
+        LeftAction.Enable();
+        MoveAction.Enable();
     }
-
-    // Update is called once per frame
-    float horizontal = 0.0f;
-    float vertical = 0.0f;
+    
+    public InputAction LeftAction;
+    public InputAction MoveAction;
+    
     void Update()
     {
-        float position = transform.position;
-        position.x = horizontal;
-        position.y = vertical;
+        Vector2 move = MoveAction.ReadValue<Vector2>();
+        Debug.Log(move);
+        Vector2 position = (Vector2)transform.position + move * 8.0f * Time.deltaTime;
         transform.position = position;
-
-        //This is the movement part of the script
-        if (Keyboard.current.leftArrowKey.isPressed)
-        {
-            horizontal = -1.0f;
-        }
-        else if (Keyboard.current.rightArrowKey.isPressed)
-        {
-            horizontal = 1.0f;
-        }
-        Debug.Log(horizontal);
-
-        if (Keyboard.current.upArrowKey.isPressed) { 
-            vertical = 1.0f;
-        }
-        else if (Keyboard.current.downArrowKey.isPressed)
-        {
-            vertical = -1.0f;
-        }
-        Debug.Log(vertical);
-
-
-        //Friction area---------------------
-        
-        //if the player has horizontal movement
-        if (horizontal > 0.0f)
-        {
-            horizontal = horizontal - 0.001f;
-        }
-        //terminates horizontal movement if it's less than 0
-        else if (horizontal <= 0.0f)
-        {
-            horizontal = 0.0f;
-        }
     }
 }
